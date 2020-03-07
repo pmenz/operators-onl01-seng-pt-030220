9c6d7e4b842ed3279469200219d7738475e7466b
Pablo Menz <p.menz.r@gmail.com>
2020-03-07 00:16:40 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,13 +3,16 @@
 
 def unsafe?(speed)
   speed=s
-  if (s>60  
+  if (s>60 || s<40)
+    puts "crazy"
+  else 
+    puts "good"
 end
 ##using ternary operator
 
 def not_safe?(speed)
 	speed=s
-  (s>60 || s<40)?
+  (s>60 || s<40)? "crazy" : "good"
 end
 	
 
